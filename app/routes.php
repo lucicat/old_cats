a7e16fb1d7e2a7b665ecaa484de17dbe6970b314
Eugene <jkramorev@yandex.ru>
2017-02-25 01:01:02 +0700

    add icons: logout, profile

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,7 @@
 <?php
 	$app->get('/', 'HomeController:sayHello')->setName('home');
 	$app->get('/auth/signup', 'AuthController:getSignUp')->setName('auth.signup');
-	$app->get('/auth/signout', 'AuthController:getSignOut');
 	$app->post('/auth/signup', 'AuthController:postSignUp');
+	$app->get('/auth/signout', 'AuthController:getSignOut')->setName('auth.signout');
 	$app->post('/auth/signin', 'AuthController:postSignIn')->setName('auth.signin');
  ?>
